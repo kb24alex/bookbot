8107396fb2b8b04825ead57e6eb7e69b9b314f7e
kb24alex <alexvoda13@yahoo.com>
2025-01-21 22:26:09 +0000

    finalized version

--- main.py
+++ main.py
@@ -3,22 +3,36 @@ def main():
     text = get_book_text(book_path)
     word_count = count_words(text)
     character_count= count_characters(text)
-    #print(word_count)
-    print(character_count)
+
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{word_count} words were found in the document")
+    print()
+    
+    for item in character_count:
+        if not item["character"].isalpha():
+            continue
+        print(f"The '{item['character']}' character was found {item['count']} times")
+    print("---End report---")
     
 def count_words(text):
     words=text.split()
     return len(words)
 
+def sort_on(char_dict):
+     return char_dict["count"]
+
 def count_characters(text):
     char_count={}
     for char in text:
-        char=char.lower()
-        if char in char_count:
+        if char.isalpha():
+            char=char.lower()
+            if char in char_count:
                 char_count[char] += 1
-        else:
+            else:
                 char_count[char] = 1
-    return char_count
+    char_list = [{'character':key, 'count':value} for key,value in char_count.items()]
+    char_list.sort(reverse = True, key=sort_on)
+    return char_list
 
 def get_book_text(path):
     with open(path) as f:
